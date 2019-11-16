38ae59e5ec605a2f0c417faf8dbe228b032c52a5
Alessandro Pogliaghi <apogliaghi@gmail.com>
2019-11-16 02:23:17 +0100

    Added FE first iteration
    Monster panel still missing

---
+++ frontend/USIHackaton2019/app/src/main/java/com/tatoalo/usihackaton2019/MainActivity.java
@@ -0,0 +1,72 @@
+package com.tatoalo.usihackaton2019;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText hp, xp;
+    ProgressBar hpValue, xpValue;
+    ImageButton dataBike, dataBus;
+    ImageView playerImage;
+    Spinner startData, stopData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        hp = (EditText) findViewById(R.id.hp);
+        xp = (EditText) findViewById(R.id.xp);
+
+        hpValue = (ProgressBar) findViewById(R.id.hpValue);
+        xpValue = (ProgressBar) findViewById(R.id.xpValue);
+
+        hp.setFocusable(false);
+        xp.setFocusable(false);
+        hpValue.setFocusable(false);
+        xpValue.setFocusable(false);
+
+        dataBike = (ImageButton) findViewById(R.id.dataTypeBike);
+        dataBike.setImageResource(R.drawable.bike);
+        dataBus = (ImageButton) findViewById(R.id.dataTypeBus);
+
+        playerImage = (ImageView) findViewById(R.id.playerImg);
+
+        //Init Values
+        hpValue.setProgress(55, true);
+        xpValue.setProgress(65, true);
+
+        Spinner startData = findViewById(R.id.dataStart);
+        Spinner stopData = findViewById(R.id.dataStop);
+        String[] items = new String[]{"Test1", "Test2", "Test3"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        startData.setAdapter(adapter);
+        stopData.setAdapter(adapter);
+
+        dataBike.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                dataBike.setBackgroundResource(R.drawable.btn_border);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+
+    }
+}
