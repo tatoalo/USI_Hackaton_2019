7d9c6ded8682d1388d6cf2def8ad54fda18142e8
Ondrej Pudiš <ondrejpudis@gmail.com>
2019-11-16 14:19:30 +0100

    return integration models from database layer

--- backend/database/monster.py
+++ backend/database/monster.py
@@ -1,24 +1,26 @@
 from typing import List
-from asyncpg import Record
 
 from .models import Monster
-from ..classes import Monster as MonsterIntegrationClass
+from ..classes import Monster as IntegrationMonster
 from .connection import database_connection
 from . import core
 
 
 @database_connection
-async def get_all_monsters(connection) -> List[Record]:
-    return await core.fetch_more(connection, [Monster])
+async def get_all_monsters(connection) -> List[Monster]:
+    response = await core.fetch_more(connection, [Monster])
+    return [IntegrationMonster(**row) for row in response]
 
 
 @database_connection
-async def get_monster(connection, *, monster_id: int) -> Record:
-    return await core.get_one(connection, [Monster], condition=Monster.c.id == monster_id)
+async def get_monster(connection, *, monster_id: int) -> Monster:
+    response = await core.get_one(connection, [Monster], condition=Monster.c.id == monster_id)
+    return IntegrationMonster(**response)
 
 
 @database_connection
-async def create_monster(connection, *, monster: MonsterIntegrationClass) -> Record:
-    return await core.create(
+async def create_monster(connection, *, monster: IntegrationMonster) -> IntegrationMonster:
+    response = await core.create(
         connection, Monster, name=monster.name, level=monster.lvl, icon=monster.icon, maximum_hp=monster.max_hp
     )
+    return IntegrationMonster(**response)
--- backend/database/user.py
+++ backend/database/user.py
@@ -1,26 +1,27 @@
-from typing import Dict, List
-from asyncpg import Record
 from sqlalchemy import select, join, literal_column
 
 from . import core
 from .models import User, UserStatistics, Fight
 from .connection import database_connection
-from ..classes import Monster
+from ..classes import Monster, User as IntegrationUser, Stats, CurrentFight
 
 USER_STATISTICS_JOIN = join(User, UserStatistics, User.c.id == UserStatistics.c.user_id)
 USER_STATISTICS_FIGHT_JOIN = join(USER_STATISTICS_JOIN, Fight, User.c.id == Fight.c.user_id)
 
 
 @database_connection
-async def get_user(connection, *, user_id: int, include_fight: bool = False) -> Record:
-    on_join = USER_STATISTICS_FIGHT_JOIN if include_fight else USER_STATISTICS_JOIN
-    on_select = (*User.c, *UserStatistics.c, *Fight.c) if include_fight else (*User.c, *UserStatistics.c)
-    query = select(on_select).select_from(on_join).where(User.c.id == user_id)
-    return await core.fetch_one(connection, query=query)
+async def get_user(connection, *, user_id: int) -> IntegrationUser:
+    query = (
+        select((*User.c, *UserStatistics.c, Fight.c.monster_id, Fight.c.monster_hp))
+        .select_from(USER_STATISTICS_FIGHT_JOIN)
+        .where(User.c.id == user_id)
+    )
+    user = await core.fetch_one(connection, query=query)
+    return IntegrationUser(**user, stats=Stats(**user, lvl=user["level"]), current_fight=CurrentFight(**user))
 
 
 @database_connection
-async def create_user(connection, *, user_name: str, user_icon: str, monster: Monster) -> Dict:
+async def create_user(connection, *, user_name: str, user_icon: str, monster: Monster) -> IntegrationUser:
     async with connection.transaction():
         user = await connection.fetchrow(
             User.insert().values(name=user_name, icon=user_icon).returning(literal_column("*"))
@@ -33,24 +34,27 @@ async def create_user(connection, *, user_name: str, user_icon: str, monster: Mo
             .values(user_id=user["id"], monster_id=monster.id, monster_hp=monster.max_hp)
             .returning(literal_column("*"))
         )
-    return {**user, **user_statistics, **user_fight}
+    return IntegrationUser(**user, stats=Stats(**user_statistics), current_fight=CurrentFight(**user_fight))
 
 
 @database_connection
-async def update_user_stats(connection, *, user_id: int, **attributes) -> List[Record]:
+async def update_user_stats(connection, *, user_id: int, **attributes) -> Stats:
     """Update statistics of the user."""
-    return await core.update(connection, UserStatistics, UserStatistics.c.user_id == user_id, **attributes)
+    response = await core.update(connection, UserStatistics, UserStatistics.c.user_id == user_id, **attributes)
+    return Stats(**response[0])
 
 
 @database_connection
-async def update_user_fight(connection, *, user_id: int, new_monster_hp: int) -> List[Record]:
+async def update_user_fight(connection, *, user_id: int, new_monster_hp: int) -> CurrentFight:
     """Update the HP of the current monster."""
-    return await core.update(connection, Fight, Fight.c.user_id == user_id, monster_hp=new_monster_hp)
+    response = await core.update(connection, Fight, Fight.c.user_id == user_id, monster_hp=new_monster_hp)
+    return CurrentFight(**response[0])
 
 
 @database_connection
-async def create_new_user_file(connection, *, user_id: int, monster: Monster) -> List[Record]:
+async def create_new_user_file(connection, *, user_id: int, monster: Monster) -> CurrentFight:
     """Create a new fight for a user by updating their Fight record and setting monster's HP to its max value."""
-    return await core.update(
+    response = await core.update(
         connection, Fight, Fight.c.user_id == user_id, monster_id=monster.id, monster_hp=monster.max_hp
     )
+    return CurrentFight(**response[0])
