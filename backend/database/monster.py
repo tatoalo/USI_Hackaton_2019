1db062b5c6214c5137290807fc7dcd8d4dcdb639
Ondrej Pudiš <ondrejpudis@gmail.com>
2019-11-16 14:34:17 +0100

    fix return data from monster queries

--- backend/database/monster.py
+++ backend/database/monster.py
@@ -7,15 +7,15 @@ from . import core
 
 
 @database_connection
-async def get_all_monsters(connection) -> List[Monster]:
+async def get_all_monsters(connection) -> List[IntegrationMonster]:
     response = await core.fetch_more(connection, [Monster])
-    return [IntegrationMonster(**row) for row in response]
+    return [IntegrationMonster(**row, lvl=row["level"], max_hp=row["maximum_hp"]) for row in response]
 
 
 @database_connection
-async def get_monster(connection, *, monster_id: int) -> Monster:
+async def get_monster(connection, *, monster_id: int) -> IntegrationMonster:
     response = await core.get_one(connection, [Monster], condition=Monster.c.id == monster_id)
-    return IntegrationMonster(**response)
+    return IntegrationMonster(**response, lvl=response["level"], max_hp=response["maximum_hp"])
 
 
 @database_connection
@@ -23,4 +23,4 @@ async def create_monster(connection, *, monster: IntegrationMonster) -> Integrat
     response = await core.create(
         connection, Monster, name=monster.name, level=monster.lvl, icon=monster.icon, maximum_hp=monster.max_hp
     )
-    return IntegrationMonster(**response)
+    return IntegrationMonster(**response, lvl=response["level"], max_hp=response["maximum_hp"])
