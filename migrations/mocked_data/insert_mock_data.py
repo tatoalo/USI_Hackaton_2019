2a6af0c6f6a056327fbfd68310c351945ffb456a
Radek Ježek <jezekra1@fit.cvut.cz>
2019-11-17 12:51:38 +0100

    Change monster names

--- migrations/mocked_data/insert_mock_data.py
+++ migrations/mocked_data/insert_mock_data.py
@@ -14,19 +14,30 @@ async def insert(users, monsters):
 
 async def insert_data():
     monsters = [
-        Monster(id=1, lvl=2, max_hp=150, icon="https://drive.google.com/uc?id=117Jvzb6PTym84CiF8qi_0Q6NYth19GqO", name="M1"),
-        Monster(id=2, lvl=1, max_hp=100, icon="https://drive.google.com/uc?id=1SBGkcKdlUqjg40B_TNqUV7F3BtnhHQSD", name="M2"),
-        Monster(id=3, lvl=4, max_hp=90, icon="https://drive.google.com/uc?id=14KilPOpzYQHOrs3tWqp-ZnFh_0rMibhM", name="M3"),
-        Monster(id=4, lvl=3, max_hp=120, icon="https://drive.google.com/uc?id=1qF7ATZ172nFpzBTya1KNy8wawuntAUjh", name="M4"),
-        Monster(id=5, lvl=8, max_hp=130, icon="https://drive.google.com/uc?id=1pdfeneCRld71lXX5A5RgSZw_LVGjECGF", name="M5"),
-        Monster(id=6, lvl=7, max_hp=140, icon="https://drive.google.com/uc?id=117Jvzb6PTym84CiF8qi_0Q6NYth19GqO", name="M6"),
-        Monster(id=7, lvl=6, max_hp=110, icon="https://drive.google.com/uc?id=1SBGkcKdlUqjg40B_TNqUV7F3BtnhHQSD", name="M7"),
-        Monster(id=8, lvl=9, max_hp=111, icon="https://drive.google.com/uc?id=14KilPOpzYQHOrs3tWqp-ZnFh_0rMibhM", name="M8"),
-        Monster(id=9, lvl=10, max_hp=20, icon="https://drive.google.com/uc?id=1qF7ATZ172nFpzBTya1KNy8wawuntAUjh", name="M9"),
-        Monster(id=10, lvl=12, max_hp=80, icon="https://drive.google.com/uc?id=1pdfeneCRld71lXX5A5RgSZw_LVGjECGF", name="M10"),
+        Monster(id=1, lvl=2, max_hp=150, icon="https://drive.google.com/uc?id=117Jvzb6PTym84CiF8qi_0Q6NYth19GqO",
+                name="Dioxider"),
+        Monster(id=2, lvl=1, max_hp=100, icon="https://drive.google.com/uc?id=1SBGkcKdlUqjg40B_TNqUV7F3BtnhHQSD",
+                name="Ozonator"),
+        Monster(id=3, lvl=4, max_hp=90, icon="https://drive.google.com/uc?id=14KilPOpzYQHOrs3tWqp-ZnFh_0rMibhM",
+                name="The iron spite particle"),
+        Monster(id=4, lvl=3, max_hp=120, icon="https://drive.google.com/uc?id=1qF7ATZ172nFpzBTya1KNy8wawuntAUjh",
+                name="Shadebrute"),
+        Monster(id=5, lvl=8, max_hp=130, icon="https://drive.google.com/uc?id=1pdfeneCRld71lXX5A5RgSZw_LVGjECGF",
+                name="The dark razordust"),
+        Monster(id=6, lvl=7, max_hp=140, icon="https://drive.google.com/uc?id=117Jvzb6PTym84CiF8qi_0Q6NYth19GqO",
+                name="Acidhound"),
+        Monster(id=7, lvl=6, max_hp=110, icon="https://drive.google.com/uc?id=1SBGkcKdlUqjg40B_TNqUV7F3BtnhHQSD",
+                name="Smokechild"),
+        Monster(id=8, lvl=9, max_hp=111, icon="https://drive.google.com/uc?id=14KilPOpzYQHOrs3tWqp-ZnFh_0rMibhM",
+                name="Dusthag"),
+        Monster(id=9, lvl=10, max_hp=20, icon="https://drive.google.com/uc?id=1qF7ATZ172nFpzBTya1KNy8wawuntAUjh",
+                name="Radioactive cloud"),
+        Monster(id=10, lvl=12, max_hp=80, icon="https://drive.google.com/uc?id=1pdfeneCRld71lXX5A5RgSZw_LVGjECGF",
+                name="Chaoscat"),
     ]
 
     users = [
-        {'name': 'Psyduck', 'icon': 'https://drive.google.com/uc?id=18Hz6uyLsnGTnUmezXtA5bGqbVYBvJJRf', 'm': monsters[2]}
+        {'name': 'Psyduck', 'icon': 'https://drive.google.com/uc?id=18Hz6uyLsnGTnUmezXtA5bGqbVYBvJJRf',
+         'm': monsters[2]}
     ]
     await insert(users, monsters)
